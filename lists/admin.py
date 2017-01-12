78bcc904a356723d3e49fbe3dda34d2e9d2d2515
nkostoulas <nkostoulas@gmail.com>
2017-01-12 17:48:27 +0000

    Updated models

--- lists/admin.py
+++ lists/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+#class ObjectAdmin(admin.ModelAdmin):
+
 admin.site.register(Category)
 admin.site.register(Object)
+admin.site.register(Choice)
--- lists/models.py
+++ lists/models.py
@@ -2,15 +2,28 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Object(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category)
 
-    def _str_(self):
+    name = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(Category)
+    description = models.TextField()
+    url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
         return self.name
+
+class Choice(models.Model):
+    
+    choice = models.ForeignKey(Object, null=True)
+    user = models.ForeignKey('auth.User')
+
+    def __str__(self):
+        return "%s, %s, %s" % (self.user, self.choice, self.choice.category)
+
