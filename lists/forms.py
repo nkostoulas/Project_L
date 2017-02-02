73bc2d6daa591e06e08e3e3c52f3e961cc925680
nkostoulas <nkostoulas@gmail.com>
2017-02-02 19:15:07 +0000

    Fixed bug of entering same choices in the form

--- lists/forms.py
+++ lists/forms.py
@@ -41,36 +41,42 @@ class ChoicesForm(forms.Form):
 		choice_4 = cleaned_data.get('choice_4')
 		choice_5 = cleaned_data.get('choice_5')
 
-		if choice_1 and choice_2 and choice_3 and choice_4 and choice_5:
-
+		if choice_2 is not None:
 			if choice_1==choice_2:
 				self._errors['choice_2'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_2']
-			elif choice_1==choice_3:
+		
+		if choice_3 is not None:
+			if choice_1==choice_3:
 				self._errors['choice_3'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_3']
-			elif choice_1==choice_4:
-				self._errors['choice_4'] = self.error_class(['Choices must be unique'])
-				del self.cleaned_data['choice_4']
-			elif choice_1==choice_5:
-				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
-				del self.cleaned_data['choice_5']
 			elif choice_2==choice_3:
 				self._errors['choice_3'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_3']
+
+		if choice_4 is not None:
+			if choice_1==choice_4:
+				self._errors['choice_4'] = self.error_class(['Choices must be unique'])
+				del self.cleaned_data['choice_4']
 			elif choice_2==choice_4:
 				self._errors['choice_4'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_4']
-			elif choice_2==choice_5:
-				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
-				del self.cleaned_data['choice_5']
 			elif choice_3==choice_4:
 				self._errors['choice_4'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_4']
+
+		if choice_5 is not None:
+			if choice_1==choice_5:
+				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
+				del self.cleaned_data['choice_5']
+			elif choice_2==choice_5:
+				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
+				del self.cleaned_data['choice_5']
 			elif choice_3==choice_5:
 				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_5']
 			elif choice_4==choice_5:
 				self._errors['choice_5'] = self.error_class(['Choices must be unique'])
 				del self.cleaned_data['choice_5']
+
 		return cleaned_data
