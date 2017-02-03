204f0f0e4170076f3c5822b5bb1f036f0f0f4edf
Schwannk <schwann.khaw@gmail.com>
2017-02-03 20:17:19 +0000

    Improved site layout and design

---
+++ reset_db.bat
@@ -0,0 +1,14 @@
+del db.sqlite3
+del projectl\migrations\*
+del lists\migrations\*
+del recommender\migrations\*
+
+python manage.py makemigrations lists
+python manage.py makemigrations recommender
+python manage.py migrate
+
+python manage.py loaddata lists/fixtures/category_db.json
+python manage.py loaddata lists/fixtures/object_db.json
+python manage.py loaddata lists/fixtures/user_db.json
+python manage.py loaddata lists/fixtures/userprofile_db.json
+python manage.py loaddata lists/fixtures/userchoicelist_db.json
